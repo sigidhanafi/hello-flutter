7822547cc3a5a0d638ca643ec08ce80cef58fe25
sigidhanafi <sigidhanafi@gmail.com>
2018-03-14 00:25:21 +0700

    create statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
@@ -12,9 +13,22 @@ class MyApp extends StatelessWidget {
           title: new Text('Hello flutter'),
         ),
         body: new Center(
-          child: new Text('Hello flutter world'),
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
